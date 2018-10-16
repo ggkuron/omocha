38ed80e9d278d713e495a3d0f0f29ac3a155cd0e
ggkuron <ggkuron@gmail.com>
2018-10-17 02:58:34 +0900

    WIP

--- Main.hs
+++ Main.hs
@@ -18,6 +18,7 @@ import Omocha.Font
 import Paths_omocha
 import Graphics.GPipe 
 import qualified Graphics.GPipe.Context.GLFW as GLFW  
+import qualified Graphics.GPipe.Context.GLFW.Input as Input
 
 import qualified Codec.Picture as P
 import qualified Codec.Picture.Types as P
@@ -44,39 +45,53 @@ getImage bmp = let img = bitmapImage bmp
 
 getJuicyPixel xs _ _ pix = let P.PixelRGBA8 r g b a = P.convertPixel pix in V4 r g b a : xs   
 
-type UniInput = (B2 Int32, V3 (B3 Float), B3 Float, B3 Float, B3 Float)
+type UniInput = (B2 Int32, V3 (B3 Float), B3 Float, B3 Float, B3 Float, B Float)
 
-windowSize  (a, _, _, _, _) = a
-modelNorm   (_, a, _, _, _) = a
-viewCamera  (_, _, a, _, _) = a 
-viewTarget  (_, _, _, a, _) = a
-viewUp      (_, _, _, _, a) = a
+windowSize  (a, _, _, _, _, _) = a
+modelNorm   (_, a, _, _, _, _) = a
+viewCamera  (_, _, a, _, _, _) = a 
+viewTarget  (_, _, _, a, _, _) = a
+viewUp      (_, _, _, _, a, _) = a
 
 
-data  RenderInput os = RenderInput {
-   riScreenSize :: V2 Int
-,  riStream :: PrimitiveArray Triangles (B3 Float, B3 Float)
-,  tex :: Texture2D os (Format RGBAFloat)
-}
+data RenderInput os = RenderInput {
+                        riScreenSize :: V2 Int, 
+                        riStream :: PrimitiveArray Triangles (B3 Float, B3 Float, B2 Float),
+                        tex :: Texture2D os (Format RGBAFloat)
+                      }
+                    | BoardInput {
+                        riScreenSize :: V2 Int, 
+                        riStream :: PrimitiveArray Triangles (B3 Float, B3 Float, B2 Float),
+                        tex :: Texture2D os (Format RGBAFloat)
+                      }
 
 data Buffers os = Buffers {
-    position :: Buffer os (B3 Float),
-    normals  :: Buffer os (B3 Float),
-    board :: Buffer os (B3 Float),
-    boardNormals :: Buffer os (B3 Float)
-}
+                              normals  :: Buffer os (B3 Float),
+                              board :: Buffer os (B3 Float, B2 Float),
+                              boardNormals  :: Buffer os (B3 Float)
+                          }
 
 
 main = runContextT GLFW.defaultHandleConfig $ do
     win <- newWindow (WindowFormatColorDepth RGBA8 Depth16) (GLFW.defaultWindowConfig "omocha")
     uniform :: Buffer os (Uniform UniInput) <- newBuffer 1  
 
-    position :: Buffer os (B3 Float) <- newBuffer 4  
+    position :: Buffer os (B3 Float, B2 Float) <- newBuffer 4  
     normals :: Buffer os (B3 Float) <- newBuffer 1  
-    board :: Buffer os (B3 Float) <- newBuffer 4  
+    board :: Buffer os (B3 Float, B2 Float) <- newBuffer 4
     boardNormals :: Buffer os (B3 Float) <- newBuffer 1  
+    tip :: Buffer os (B2 Int32) <- newBuffer 32
+
+    writeBuffer (normals) 0 [V3 0 0 1]
+    writeBuffer (board) 0 [
+                              (V3 1 0 1, V2 1 1),
+                              (V3 1 0 (-1), V2 1 0),
+                              (V3 (-1) 0 1, V2 0 1),
+                              (V3 (-1) 0 (-1), V2 0 0)
+                          ] 
+    writeBuffer (normals) 0 [V3 0 1 0]
+    writeBuffer (boardNormals) 0 [V3 0 1 0]
     let buffers = Buffers {
-        position,
         normals,
         board,
         boardNormals
@@ -84,7 +99,11 @@ main = runContextT GLFW.defaultHandleConfig $ do
     let makePrimitives p n = do   
           pArr <- newVertexArray p  
           nArr <- newVertexArray n  
-          return $ toPrimitiveArrayInstanced TriangleStrip (,) pArr nArr   
+          return $ 
+            toPrimitiveArrayInstanced 
+            TriangleStrip 
+            (\(p, u) n -> (p, n, u))
+            pArr nArr 
 
     font <- loadFont "VL-PGothic-Regular.ttf"
 
@@ -103,10 +122,10 @@ main = runContextT GLFW.defaultHandleConfig $ do
                                   shader $ RenderInput size prims tex
                               ]
 
-    (input, inputSink) <- liftIO $ E.external (False, False, False, False)
+    (input, inputSink) <- liftIO $ E.external ((False, False, False, False), 0.0)
     network <- liftIO $ E.start $ mdo
         target <- E.transfer (V3 0 0 0)
-                             (\(keyH, keyJ, keyK, keyL) t -> 
+                             (\((keyH, keyJ, keyK, keyL), _) t -> 
                                  t&_x+~(if 
                                          | keyL -> -0.02
                                          | keyH -> 0.02
@@ -117,14 +136,29 @@ main = runContextT GLFW.defaultHandleConfig $ do
                                          | otherwise ->  0)
                              ) 
                              input
+        target' <- E.effectful1  
+            (\target -> do {
+                putStrLn "working";
+                action <- return $ writeBuffer (position) 0 
+                [
+                    (target&_x+~1&_y*~(-1)&_y+~1, V2 1 1),
+                    (target&_x+~1&_y*~(-1)&_y-~1, V2 1 0),
+                    (target&_x-~1&_y*~(-1)&_y+~1, V2 0 1),
+                    (target&_x-~1&_y*~(-1)&_y-~1, V2 0 0)
+                ] :: IO (ContextT GLFW.Handle os IO ());
+                return action
+             }
+            ) target
+
         camera' <- E.transfer (V3 0 0.25 1) 
                               (\t camera -> 
                                   t&_y+~(5)
                                    &_z-~(7.5) 
                               ) 
                               target
+        time <- E.transfer (0.0) (\(_, t) _ -> t) input
 
-        return $ renderFrame win uniform buffers renderings <$> camera' <*> target
+        return $ renderFrame win uniform buffers renderings <$> camera' <*> target <*> time
 
     fix $ \loop -> do
         closeRequested <- GLFW.windowShouldClose win 
@@ -133,7 +167,9 @@ main = runContextT GLFW.defaultHandleConfig $ do
                        <*> keyIsPressed win GLFW.Key'J
                        <*> keyIsPressed win GLFW.Key'K
                        <*> keyIsPressed win GLFW.Key'L
-        join $ liftIO $ inputSink input >> network 
+
+        Just t' <- liftIO $ GLFW.getTime
+        join $ liftIO $ inputSink (input, t') >> network 
 
         unless (closeRequested == Just True || closeKeyPressed) loop 
 
@@ -143,38 +179,32 @@ renderFrame :: Window os RGBAFloat Depth
                -> (V2 Int -> [Render os ()])
                -> V3 Float
                -> V3 Float
+               -> Double
                -> ContextT GLFW.Handle os IO ()
-renderFrame win uniform buffers renderings camera target= do
+renderFrame win uniform buffers renderings camera target t = do
   size <- getFrameBufferSize win
   let viewUp = V3 0 1 0 
       normMat = identity
-      uni = (fromIntegral <$> size, normMat, camera, target, viewUp) 
+      uni = (fromIntegral <$> size, normMat, camera, target, viewUp, realToFrac t) 
 
   writeBuffer uniform 0 [uni]
-  writeBuffer (position buffers) 0 [ target&_x+~1&_y*~(-1)&_y+~1,
-                                     target&_x+~1&_y*~(-1)&_y-~1,
-                                     target&_x-~1&_y*~(-1)&_y+~1,
-                                     target&_x-~1&_y*~(-1)&_y-~1
-                                   ]
-  writeBuffer (normals buffers) 0 [V3 0 0 1]
-  writeBuffer (board buffers) 0 [V3 1 0 1, V3 1 0 (-1), V3 (-1) 0 1, V3 (-1) 0 (-1)]        
-  writeBuffer (normals buffers) 0 [V3 0 1 0]
-  writeBuffer (boardNormals buffers) 0 [V3 0 1 0]
-
   mapM_ render (renderings size)
   swapWindowBuffers win
 
 
 keyIsPressed win k = maybe False (== GLFW.KeyState'Pressed) <$> GLFW.getKey win k
         
-boardShader :: ((V3 VFloat, V3 VFloat) -> (V3 (S V Float), V3 VFloat, V2 (S V Float))) -> Window os RGBAFloat Depth -> Buffer os (Uniform UniInput) -> Shader os (RenderInput os) ()
+boardShader :: ((V3 VFloat, V3 VFloat, V2 VFloat) -> (V3 (S V Float), V3 VFloat, V2 (S V Float))) 
+               -> Window os RGBAFloat Depth 
+               -> Buffer os (Uniform UniInput) 
+               -> Shader os (RenderInput os) ()
 boardShader pick win uniform = do
     uni <- getUniform (const (uniform, 0))
     boards <- fmap pick <$> toPrimitiveStream riStream
     let projectedSides = proj uni <$> boards
         filterMode = SamplerFilter Linear Linear Linear (Just 16)
         edge = (pure ClampToEdge, 1.0)
-    samp <- newSampler2D (\ri -> (tex ri, filterMode, edge))
+    samp <- newSampler2D $ \ri -> (tex ri, filterMode, edge)
 
     uv <- rasterize (\ri -> (Front, ViewPort (V2 0 0) (riScreenSize ri), DepthRange 0 1) ) projectedSides
     let litFrags = light samp <$> uv
@@ -189,13 +219,11 @@ boardShader pick win uniform = do
 light :: ColorSampleable c => Sampler2D (Format c) -> (t, V2 (S F Float)) -> ColorSample F c
 light samp (normal, uv) = sample2D samp SampleAuto (Just 1) Nothing uv 
 
-makeSide :: Fractional a => (V3 a, V3 a) -> (V3 a, V3 a, V2 a)
-makeSide (p, normal) = (p&_y+~1, normal, uv)    
-  where uv = (p^._xy + 1) / 2
+makeSide :: Fractional a => (V3 a, V3 a, V2 a) -> (V3 a, V3 a, V2 a)
+makeSide (p, normal, uv) = (p&_y+~1, normal, uv)
 
-makeBoard :: Fractional a => (V3 a, V3 a) -> (V3 a, V3 a, V2 a)
-makeBoard (p, normal) = (p, normal, uv)
-  where uv = (p^._xz + 1) / 2
+makeBoard :: Fractional a => (V3 a, V3 a, V2 a) -> (V3 a, V3 a, V2 a)
+makeBoard (p, normal, uv) = (p, normal, uv)
 
 proj uni (V3 px py pz, normal, uv) =   
   let modelViewProj = perspective (pi/3) (let V2 w h = windowSize uni in (toFloat w) / (toFloat h)) 1 (-1)
